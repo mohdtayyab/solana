0f0d63efb90f392795c81b9bc086fd0d0aa39b3c
Mohd Tayyab <ta8041936@gmail.com>
2021-08-18 13:41:21 +0530

    Update export-github-repo.sh

--- .travis/export-github-repo.sh
+++ .travis/export-github-repo.sh
@@ -33,10 +33,10 @@ git clone https://"$GITHUB_TOKEN"@github.com/mohdtayyab/"$repo_name" .github_exp
 git filter-repo --subdirectory-filter "$subdir" --target .github_export/"$repo_name"
 git remote set-url origin https://"$GITHUB_TOKEN"@github.com/mohdtayyab/"$repo_name"
 echo "origin"
-git fetch origin master:tmp
-git rebase tmp
-#git fetch origin master
-#git pull origin master --allow-unrelated-histories 
+#git fetch origin master:tmp
+#git rebase tmp
+git fetch origin master
+git pull origin master  
 #git merge origin master
 #git add .
 # git rm --cached .github_export/"$repo_name"
