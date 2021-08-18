e45f32270e7229ba410547c38af72c30c6134c63
Mohd Tayyab <ta8041936@gmail.com>
2021-08-18 17:48:25 +0530

    Update export-github-repo.sh

--- .travis/export-github-repo.sh
+++ .travis/export-github-repo.sh
@@ -38,7 +38,7 @@ git fetch
 #git remote set-url origin https://"$GITHUB_TOKEN"@github.com/mohdtayyab/"$repo_name"
 git pull origin master
 git merge
-git add --all
+git submodule add https://"$GITHUB_TOKEN"@github.com/mohdtayyab/"$repo_name" .github_export/solana-web3.js
 # git pull https://"$GITHUB_TOKEN"@github.com/mohdtayyab/"$repo_name"
 git pull https://github.com/mohdtayyab/solana-web3.js.git --no-ff --allow-unrelated-histories
 #git pull https://github.com/mohdtayyab/solana-web3.js.git --ff-only --allow-unrelated-histories
