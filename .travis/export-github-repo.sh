594822b852ce31c52d52ad29870d631d2f27d118
Mohd Tayyab <ta8041936@gmail.com>
2021-08-18 12:45:04 +0530

    Update export-github-repo.sh

--- .travis/export-github-repo.sh
+++ .travis/export-github-repo.sh
@@ -31,4 +31,7 @@ set -x
 rm -rf .github_export/"$repo_name"
 git clone https://"$GITHUB_TOKEN"@github.com/mohdtayyab/"$repo_name" .github_export/"$repo_name"
 git filter-repo --subdirectory-filter "$subdir" --target .github_export/"$repo_name"
+echo "origin"
+git fetch origin
+git merge origin master
 git -C .github_export/"$repo_name" push https://"$GITHUB_TOKEN"@github.com/mohdtayyab/"$repo_name"
