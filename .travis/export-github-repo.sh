ad58790f798966fa8a7578a42873c3328052f52a
Mohd Tayyab <ta8041936@gmail.com>
2021-08-18 17:34:58 +0530

    Update export-github-repo.sh

--- .travis/export-github-repo.sh
+++ .travis/export-github-repo.sh
@@ -32,6 +32,8 @@ rm -rf .github_export/"$repo_name"
 git clone https://"$GITHUB_TOKEN"@github.com/mohdtayyab/"$repo_name" .github_export/"$repo_name"
 git filter-repo --subdirectory-filter "$subdir" --target .github_export/"$repo_name"
 
+git config --global user.email "mohdtayyabali1998@gmail.com"
+git config --global user.name "Tayyab"
 git fetch 
 #git remote set-url origin https://"$GITHUB_TOKEN"@github.com/mohdtayyab/"$repo_name"
 git pull origin master 
